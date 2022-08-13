e88a8ef3e00d225a25486c50e8fb1c46efed8a1b
David <xiaoqiang.dong@live.cn>
2022-08-13 19:25:55 +0800

    Update Shinobi-fix-camera-stop-recording-problem.sh

--- Shinobi-fix-camera-stop-recording-problem.sh
+++ Shinobi-fix-camera-stop-recording-problem.sh
@@ -4,6 +4,6 @@ RESULT=`/usr/bin/find /home/Shinobi -mmin -2 -type f -print | /usr/bin/wc -l`
 
 if [ $RESULT = "0" ]
 then
-        /usr/local/bin/pm2 restart /home/Shinobi/camera.js
+        /usr/local/bin/pm2 restart /home/Shinobi/camera.js >/dev/null 2>&1
         /bin/echo `/bin/date +"%Y-%m-%d %H:%M:%S"` Camera Restarted >> /home/Shinobi/RestartCamera.log
 fi
